6430656eb4b5cf96c3671ab96be0b666ad31af88
DayalArya <58099966+DayalArya@users.noreply.github.com>
2021-04-14 18:43:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/database');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
